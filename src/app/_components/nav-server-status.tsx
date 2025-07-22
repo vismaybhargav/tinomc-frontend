42268e376ac4721a3f0d91794d99ec7280bafd5a
Vismay Bhargav <vismay.bhargav@gmail.com>
2025-07-22 03:31:02 -0700

    fix: server status light mode fix

--- src/app/_components/nav-server-status.tsx
+++ src/app/_components/nav-server-status.tsx
@@ -10,14 +10,22 @@ export default function NavServerStatus() {
 
     return (
         <SidebarMenu>
-            <SidebarMenuItem className={`group ${isServerOn ? "bg-green-900" : "bg-red-900"} py-2 rounded-sm group-data-[collapsible=icon]:rounded-full`}>
+            <SidebarMenuItem className={`
+                group ${isServerOn ? "dark:bg-green-900 bg-green-200" : "bg-red-200 dark:bg-red-900"} 
+                py-2 rounded-sm group-data-[collapsible=icon]:rounded-full`
+            }>
                 <div className="flex justify-between mx-2 items-center text-sm">
 
                     <span className="group-data-[collapsible=icon]:hidden">Server Status</span>
 
                     <span className="relative size-3 align-center group-data-[collapsible=icon]:mx-auto">
-                        {isServerOn && <span className="absolute inline-flex h-full w-full rounded-full animate-ping bg-green-500 opacity-75"></span>}
-                        <span className={`absolute inline-flex size-3 rounded-full ${isServerOn ? "bg-green-500" : "bg-red-900 border-2 border-red-500"}`}></span>
+                        {isServerOn && 
+                            <span className="
+                                absolute inline-flex h-full w-full rounded-full animate-ping bg-green-500 opacity-75"></span>
+                        }
+                        <span className={`
+                            absolute inline-flex size-3 rounded-full 
+                            ${isServerOn ? "bg-green-500" : "dark:bg-red-900 border-2 border-red-500"}`}></span>
                     </span>
 
                 </div>
